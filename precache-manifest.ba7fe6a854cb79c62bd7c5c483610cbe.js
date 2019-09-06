d8cd2909b5a7af61867ff3064834f24ae6f85b93
Sergei Vitkovskiy <sergei.vitkovskiy@ssa.group>
2019-09-06 18:58:39 +0300

    test

---
+++ precache-manifest.ba7fe6a854cb79c62bd7c5c483610cbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c3c4572d95916d1e178bebd26c5fe9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a304d9503f48d2ab934",
+    "url": "./static/css/main.9f288cac.chunk.css"
+  },
+  {
+    "revision": "04ab9a5fde475fd69d5d",
+    "url": "./static/js/2.eee168fa.chunk.js"
+  },
+  {
+    "revision": "5a304d9503f48d2ab934",
+    "url": "./static/js/main.31f90501.chunk.js"
+  },
+  {
+    "revision": "f1334f2cc097e1ae14b2",
+    "url": "./static/js/runtime~main.22e3d947.js"
+  },
+  {
+    "revision": "8fb9c364573459fa67d50aabef7930bb",
+    "url": "./static/media/smile.8fb9c364.svg"
+  },
+  {
+    "revision": "e58b1ca5d2bc214b8eaa3ce819c01a86",
+    "url": "./static/media/smile_.e58b1ca5.svg"
+  }
+]);
